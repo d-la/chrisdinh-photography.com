fea73bb5779cacad89b050737fb2b690b3b3ac08
Daniel La <la.danieln@gmail.com>
2019-04-01 17:43:48 -0400

    Require new packages, added error handling for uncaught requests

--- api/app.js
+++ api/app.js
@@ -1,12 +1,25 @@
 let express = require('express'),
-    bodyParser = require('body-parser');
+    bodyParser = require('body-parser'),
+    passport = require('passport'),
+    localStrategy = require('passport-local'),
+    cors = require('cors');
 
 const app = express();
 const port = process.env.port || 3000;
 
+app.use(cors());
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
+
+// Routes will go here
+
+
+// Error handling
+app.use( (req, res, next) => {
+    let error = new Error("Not found");
+    error.status = 404;
+    next(error);
+});
 
 app.listen(port, () => {
-    console.log('Server is running!');
+    console.log(`Server is running on port ${port}`);
 })
