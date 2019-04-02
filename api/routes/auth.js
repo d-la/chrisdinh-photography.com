16fbea14d8e462c4726b93361fa08044a676c351
Daniel La <la.danieln@gmail.com>
2019-04-01 21:17:46 -0400

    Sign in route added

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const router = express.Router();
-const { signUp } = require('../handlers/auth');
+const { signUp, signIn } = require('../handlers/auth');
 
 router.post('/signup', signUp);
+router.post('/signin', signIn);
 
 module.exports = router;
