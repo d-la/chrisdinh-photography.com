817d301f4f59841791989a79aca59a5425552783
Daniel La <la.danieln@gmail.com>
2019-04-01 21:17:31 -0400

    signIn function added

--- api/handlers/auth.js
+++ api/handlers/auth.js
@@ -1,8 +1,44 @@
 const database = require('../models');
 const jwt = require('jsonwebtoken');
 
-exports.signIn = function(){
-
+/**
+ * Find the user
+ * Check if their password matches the hash in the server
+ * Log them in if it matches
+ */
+exports.signIn = async function(req, res, next){
+    try {
+        let user = await database.User.findOne({
+            email: req.body.email
+        });
+        let { id, email, profileImageUrl } = user;
+        let isMatch = await user.comparePassword(req.body.password);
+        
+        if (isMatch === true){
+            let token = jwt.sign({
+                id,
+                email,
+                profileImageUrl
+            }, process.env.SECRET_KEY);
+    
+            return res.status(200).json({
+                id,
+                username,
+                profileImageUrl,
+                token
+            });
+        } else {
+            return next({
+                status: 400,
+                message: 'Invalid email/password combination.'
+            });
+        }
+    } catch (error){
+        return next({
+            status: 400,
+            message: 'Invalid email/password combination.'
+        });
+    }
 }
 
 exports.signUp = async function(req, res, next){
